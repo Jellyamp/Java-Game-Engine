6f05346670446ac92fcf58f0129345a70a20665a
Dustyamp <amprulesgreatly@gmail.com>
2016-02-06 22:32:29 -0800

    Added texture loading

--- gameEngine/src/engineTester/MainGameLoop.java
+++ gameEngine/src/engineTester/MainGameLoop.java
@@ -2,11 +2,13 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -32,8 +34,17 @@ public class MainGameLoop {
 				3, 1, 2		// Bottom right triangle (V3, V1, V2)
 		};
 		
+		float[] textureCoords = {
+				0, 0,	//V0
+				0, 1,	//V1
+				1, 1,	//V2
+				1, 0 	//V3
+		};
+		
 		// Create the model
-		RawModel model = loader.loadToVAO(vertices, indices);
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		// Main game loop
 		while(!Display.isCloseRequested()) {
@@ -41,7 +52,7 @@ public class MainGameLoop {
 			// game logic
 			// Display the model
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 			
